ac8644a58f668f27f34196d5dc680ae91f626f5c
crazyplum <crazyplum24@gmail.com>
2014-07-21 17:54:24 +0800

    modified build_lexicon for kimo

--- build_lexicon.py
+++ build_lexicon.py
@@ -21,85 +21,79 @@ categories = []
 
 # calculate occurrences of patterns
 def count_patterns(categories, condition={}):
-	patCount = defaultdict(Counter)
-	for c in categories:
-		# patCount[c] = Counter()
-		query = { config.category: c }
-		query.update(condition)
-		# get udocIDs
-		for mdoc in co_docs.find(query):
-			udocID = mdoc['udocID']
-			## get patterns
-			for mpat in co_pats.find( {'udocID': udocID} ):
-				pat = mpat['pattern'].lower()
-				patCount[pat][c] += 1
-	return patCount	
+    patCount = defaultdict(Counter)
+    for mdoc in co_pats.find():
+        patCount[mdoc['pattern']][mdoc['emotion']] += 1
+    return patCount 
 
 # build lexicon storing pattern occurrence
 def build_lexicon(patCount, categories):
-	# for c in categories:
-	for p in patCount:
-		logging.debug('insert pattern %s into %s' % (color.render(p,'g'), color.render(co_lexicon.full_name,'lightyellow') ))
+    # for c in categories:
+    for p in patCount:
+        logging.debug('insert pattern %s into %s' % (color.render(p,'g'), color.render(co_lexicon.full_name,'lightyellow') ))
 
-		mdoc = {
-			'pattern': p,
-			'count': dict(patCount[p]),
-			'total_count': sum(patCount[p].values())
-		}
+        mdoc = {
+            'pattern': p,
+            'count': dict(patCount[p]),
+            'total_count': sum(patCount[p].values())
+        }
 
-		co_lexicon.insert( mdoc )
+        co_lexicon.insert( mdoc )
 
 if __name__ == '__main__':
 
 
-	program = __file__.split('.py')[0]
+    program = __file__.split('.py')[0]
 
-	import getopt
-	try:
-		opts, args = getopt.getopt(sys.argv[1:],'hov',['help','overwrite','verbose'])
-	except getopt.GetoptError:
-		config.help(program, exit=2)
+    import getopt
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],'hov',['help','overwrite','verbose'])
+    except getopt.GetoptError:
+        config.help(program, exit=2)
 
-	### read options
-	for opt, arg in opts:
-		if opt in ('-h', '--help'): config.help(program)
-		elif opt in ('-o','--overwrite'): config.overwrite = True
-		elif opt in ('-v','--verbose'): config.verbose = True
+    ### read options
+    for opt, arg in opts:
+        if opt in ('-h', '--help'): config.help(program)
+        elif opt in ('-o','--overwrite'): config.overwrite = True
+        elif opt in ('-v','--verbose'): config.verbose = True
 
-	loglevel = logging.DEBUG if config.verbose else logging.INFO
-	logging.basicConfig(format='[%(levelname)s] %(message)s', level=loglevel)
+    loglevel = logging.DEBUG if config.verbose else logging.INFO
+    logging.basicConfig(format='[%(levelname)s] %(message)s', level=loglevel)
 
 
-	## src
-	co_docs = db[config.co_docs_name]
-	co_pats = db[config.co_pats_name]
-	co_cate = db[config.co_category_name]
-	## dest
-	co_lexicon = db[config.co_lexicon_name]
+    ## src
+#   co_docs = db[config.co_docs_name]
+    co_pats = db[config.co_pats_name]
+#   co_cate = db[config.co_category_name]
+    ## dest
+    co_lexicon = db[config.co_lexicon_name]
 
-	### check whether destination collection is empty or not
-	dest_cos = [co_lexicon]
-	dest_cos_status = {co.name : co.count() for co in dest_cos}
-	logging.info('current collection status: ' + json.dumps(dest_cos_status))
-	if sum(dest_cos_status.values()) > 0 and not config.overwrite:
-		logging.warn('use --overwrite or -o to drop current data and insert new one')
-		exit(-1)
-	elif sum(dest_cos_status.values()) > 0 and config.overwrite:
-		# logging.warn('overwrite mode, will drop all data in ' + )
-		print >> sys.stderr, 'drop all data in',', '.join(dest_cos_status.keys()), '(Y/n)? ', 
-		if raw_input().lower().startswith('n'): exit(-1)
-		else: 
-			for co in dest_cos: co.drop()
+    ### check whether destination collection is empty or not
+    dest_cos = [co_lexicon]
+    dest_cos_status = {co.name : co.count() for co in dest_cos}
+    logging.info('current collection status: ' + json.dumps(dest_cos_status))
+    if sum(dest_cos_status.values()) > 0 and not config.overwrite:
+        logging.warn('use --overwrite or -o to drop current data and insert new one')
+        exit(-1)
+    elif sum(dest_cos_status.values()) > 0 and config.overwrite:
+        # logging.warn('overwrite mode, will drop all data in ' + )
+        print >> sys.stderr, 'drop all data in',', '.join(dest_cos_status.keys()), '(Y/n)? ', 
+        if raw_input().lower().startswith('n'): exit(-1)
+        else: 
+            for co in dest_cos: co.drop()
 
-	index_check_list = [(co_docs, config.category), (co_pats, 'udocID')]
-	util.check_indexes(check_list=index_check_list, verbose=config.verbose)
-	
-	logging.info('fetch categories from %s' % (color.render(co_cate.full_name, 'ly')))
-	categories = sorted([x[config.category] for x in co_cate.find({'label':config.category})])
+    #index_check_list = [(co_docs, config.category), (co_pats, 'udocID')]
+    index_check_list = []
+    util.check_indexes(check_list=index_check_list, verbose=config.verbose)
+    
+#   logging.info('fetch categories from %s' % (color.render(co_cate.full_name, 'ly')))
+#   categories = sorted([x[config.category] for x in co_cate.find({'label':config.category})])
+    import json
+    categories = list(json.load(open('emoID', 'r')).values())
 
-	logging.info('count patterns')
-	# patCount = count_patterns(categories, condition={'ldocID': {'$lt': 800}} )
-	patCount = count_patterns(categories)
+    logging.info('count patterns')
+    # patCount = count_patterns(categories, condition={'ldocID': {'$lt': 800}} )
+    patCount = count_patterns(categories)
 
-	logging.info('build pattern lexicon at %s' % (color.render(co_lexicon.full_name, 'ly')))
-	build_lexicon(patCount, categories)
+    logging.info('build pattern lexicon at %s' % (color.render(co_lexicon.full_name, 'ly')))
+    build_lexicon(patCount, categories)
