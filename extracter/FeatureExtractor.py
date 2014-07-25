407c2a1af85ec18513f51d3624f1bc0f22b60eed
maxis <maxis1718@gmail.com>
2014-07-25 17:39:36 +0800

    Add **kwargs and Add min_count options

--- extracter/FeatureExtractor.py
+++ extracter/FeatureExtractor.py
@@ -7,11 +7,17 @@ class FeatureExtractor(object):
 	"""
 	Extract Feature from mongodb based on sentence/document
 	"""
-	def __init__(self, mongo_addr="doraemon.iis.sinica.edu.tw", db_name="kimo", verbose=True):
-		self._db = pymongo.Connection(mongo_addr)[db_name]
+	def __init__(self, **kwargs):
 
-		# set logging level
-		loglevel = logging.DEBUG if verbose else logging.INFO
+		try:
+			self._db = pymongo.Connection(kwargs['mongo_addr'])[kwargs['db_name']]
+		except KeyError, e:
+			logging.error('mongo_addr and db_name are both necessary for create a mongo connection')
+			raise e
+
+		## set logging level
+		## default level: INFO
+		loglevel = logging.DEBUG if 'verbose' in kwargs and kwargs['verbose'] == True else logging.INFO
 		logging.basicConfig(format='[%(levelname)s] %(message)s', level=loglevel)
 
 
@@ -28,7 +34,7 @@ class FeatureExtractor(object):
 		setting_id = str(self._db['features.settings'].insert(settings))
 		return setting_id
 
-	def PatternEmotion(self, options={}):
+	def PatternEmotion(self, **kwargs):
 		"""
 		create pattern-emotion feature
 		> feature dimention: number of emotions
@@ -39,11 +45,17 @@ class FeatureExtractor(object):
 		settings = {
 			'feature_name': 'pattern_emotion',
 		}
-		settings.update(options)
+		settings.update(kwargs)
+
+		logging.info('save current setting: %s', json.dumps(settings))
 		setting_id = self._SaveSettings(settings)
 
 		## sentence-based
 		logging.info('start extracting features')
+
+		# detect and set min_count
+		# min_count matters only if min_count > 1
+		min_count = 0 if 'min_count' not in kwargs else kwargs['min_count']
 
 		for sent_mdoc in self._db['sents'].find():
 			features = Counter()
@@ -51,11 +63,16 @@ class FeatureExtractor(object):
 
 				## fetch lexicon to get dist
 				lexicon_mdoc = self._db['lexicon.nested'].find_one({'pattern': pat_mdoc['pattern'] })
+				## no data fethced
 				if not lexicon_mdoc:
+					continue
+				## total count is less than min_count threshold
+				elif lexicon_mdoc['total_count'] < min_count:
 					continue
 				else:
 					## aggregate counts
-					weight = pat_mdoc['weight'] if 'weighted' in options and options['weighted'] == True else 1.0
+					## detect and set weight
+					weight = pat_mdoc['weight'] if 'weighted' in kwargs and kwargs['weighted'] == True else 1.0
 					for emotion, count in lexicon_mdoc['count'].items():
 						features[emotion] += count*weight
 
@@ -76,11 +93,9 @@ class FeatureExtractor(object):
 
 if __name__ == '__main__':
 	
+	# from FeatureExtractor import FeatureExtractor
+
 	Ext = FeatureExtractor(mongo_addr="doraemon.iis.sinica.edu.tw", db_name="kimo", verbose=False)
 	
-	options = {
-		'weighted': True
-	}
-
-	Ext.PatternEmotion(options)
+	Ext.PatternEmotion(weighted=True, min_count=4)
 
