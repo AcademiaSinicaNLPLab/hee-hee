f2adf3c9d1747b8f45bf81f3c084d919633d4f1a
Maxis <maxis1718@gmail.com>
2014-07-26 03:24:09 +0800

    Add kwargs and logging

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 import sys
 sys.path.append('/tools/CKIPServer')
-#sys.path.append('/home/plum/CKIPServer')
+
+import logging
+
 from CKIPClient import *
 import os, codecs
 
 from functools import wraps
 import signal, errno
 
+from sys import stdout
 
 class TimeoutError(Exception):
     pass
@@ -31,31 +34,63 @@ def timeout(seconds=10, error_messages=os.strerror(errno.ETIME)):
     return decorator
 
 
-options = {'divide': 1000,\
-           'encoding': 'UTF-8',\
-           'port': 1501,\
-           'pos': True,\
-           'server': 'localhost',\
-           'uwe': True,\
-           'xml': False}
-
-from sys import stdout
-
 class Tokenizer:
-    def __init__(self, opt=options):
-        self.srv = CkipSrv("wordseg","1234",server=opt['server'], port=opt['port'])
+    """
+    A python wrap for CKIP tokenizer under the client-server architecture.
+    """
+    options = { 'server': 'doraemon.iis.sinica.edu.tw',
+                'port': 1502,
+
+                'divide': 1000,
+                'encoding': 'UTF-8',
+                'pos': True,
+                'uwe': True,
+                'xml': False }    
+
+    def __init__(self, **kwargs):
+        """
+        [options]
+        server  :CKIP server address
+                 default: doraemon.iis.sinica.edu.tw
+        port    :CKIP server port
+                 default: 1502
+        """
+
+        ## update options
+        self.options.update(kwargs)
+
+        ## set server, default: doraemon
+        # server = 'doraemon.iis.sinica.edu.tw' if 'server' not in options else options['server']
+        ## set port, default: 1502
+        # port = 1502 if 'port' not in options else options['port']
+        # connect to CkipServer
+        self.srv = CkipSrv("wordseg","1234", server=self.options['server'], port=self.options['port'])
+
+        ## set logging level
+        ## default level: INFO
+        loglevel = logging.DEBUG if 'verbose' in options and options['verbose'] == True else logging.INFO
+        logging.basicConfig(format='[%(levelname)s] %(message)s', level=loglevel)
 
     @timeout(30)
-    def tokenizeStr(self, s):
+    def tokenizeStr(self, text):
+        """
+        input: plain text
+        output: tokenized results
+        """
         try:
-            res = self.srv.segStr(s, options)
+            res = self.srv.segStr(text, self.options)
         except TimeoutError:
-            print "Time out"
+            logging.warn("tokenizeStr timeout")
             return None
         else:
+            logging.debug("got tokenized result: %s" % (res.strip().decode('utf-8')))
             return res
 
     def tokenizeFile(self, infile, outfile):
+        """
+        input: an file containing text (separated by '\\n')
+        output: save tokenized text in file
+        """
         with codecs.open(infile, 'r', 'utf-8') as f:
             lines = f.read().strip().split('\n')
         
@@ -63,8 +98,8 @@ class Tokenizer:
 
         for i, l in enumerate(lines):    
             print str(i) + " " + l.encode('utf-8')
-#            stdout.write("\r%d %s" % (i, l.encode('utf-8')))
-#            stdout.flush()
+            # stdout.write("\r%d %s" % (i, l.encode('utf-8')))
+            # stdout.flush()
 
             res = self.tokenizeStr(l.encode('utf-8'))
             if res is not None:
@@ -72,8 +107,13 @@ class Tokenizer:
         outf.close()
 
 if __name__ == "__main__":
-    tokenizer = Tokenizer()
-    #bad sentence
-    print tokenizer.tokenizeStr('如果你需要時間好好冷靜的思考.......沒關係......我願意等你.......無論多久我都會等你........等你準備好了.........等你願意見我.......我不會再讓你擔心.......我也會按時吃飯........也會好好照顧自己.......你說的我都答應你........真的.......我說的都是真的.........請你相信 我.......... 小黑豬不會和小白豬分開的......就算有........也只是短暫的')
+
+    tknz = Tokenizer()
+
+    # bad sentence
+    # print tknz.tokenizeStr('如果你需要時間好好冷靜的思考.......沒關係......我願意等你.......無論多久我都會等你........等你準備好了.........等你願意見我.......我不會再讓你擔心.......我也會按時吃飯........也會好好照顧自己.......你說的我都答應你........真的.......我說的都是真的.........請你相信 我.......... 小黑豬不會和小白豬分開的......就算有........也只是短暫的')
+    
+    # test sentence
+    print tknz.tokenizeStr('我在臺灣清華大學念碩士')
 
 
