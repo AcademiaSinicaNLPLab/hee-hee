6d1d628fd6901e2a7125e49ab9846273f4b0856c
crazyplum <crazyplum24@gmail.com>
2014-07-31 15:21:59 +0800

    add stuff_db.py

---
+++ mu_oc/stuff_db.py
@@ -0,0 +1,21 @@
+import pymongo
+import os, codecs
+
+if __name__ == '__main__':
+
+    co = pymongo.Connection('doraemon.iis.sinica.edu.tw')['kimo']['mutual_occurrence']
+
+
+    with codecs.open('../../mc_new.txt', 'r', 'utf-8') as f:
+        buf = []
+        for i, line in enumerate(f):
+            l = line.split('\t')
+            mdoc = {
+                    'first': l[0],
+                    'second': l[1],
+                    'count': l[2]
+            }
+            buf.append(mdoc)
+            if (i+1) % 1000 == 0:
+                co.insert(buf)
+                buf = []
