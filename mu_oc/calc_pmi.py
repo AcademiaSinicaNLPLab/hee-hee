6eaba36c61d571096e03d11cfe5d7be8f1307bac
crazyplum <crazyplum24@gmail.com>
2014-07-31 18:20:04 +0800

    fix bugs and try to accelerate

--- mu_oc/calc_pmi.py
+++ mu_oc/calc_pmi.py
@@ -1,21 +1,30 @@
+from __future__ import division
+
 import pymongo
 import math
 
+from collections import defaultdict
 
-db = pymongo.Connection('doraemon.iis.sinica.edu.tw')
+db = pymongo.Connection('doraemon.iis.sinica.edu.tw')['kimo']
 co_f = db['tfidf.emotermcount']
 co_mc = db['mutual_occurrence']
 co_PMI = db['PMI']
 
 def oc(term):
-    return sum(map(lambda x: x['count'], co_f.find({'term':term},{'_id':0, 'count':1}.batch_size(40))))
+    return sum(map(lambda x: x['count'], co_f.find({'term':term},{'_id':0, 'count':1}).batch_size(40)))
 
 
 if __name__ == '__main__':
     
     buf = []
-    for x in co_mc.find().batch_size(1000): 
-        pmi = math.log(x['count'] / (oc(x['first']) * oc(x['second'])))
+    quick = defaultdict(int)
+    for x in co_mc.find().batch_size(10000):
+        #print x['count'], oc(x['first']), oc(x['second'])
+        
+        quick[x['first']] = oc(x['first']) if quick[x['first']] == 0 else quick[x['first']]
+        quick[x['second']] = oc(x['second']) if quick[x['second']] == 0 else quick[x['second']]
+
+        pmi = math.log(x['count'] / quick[x['first']] * quick[x['second']])
         mdoc = {
                 'first': x['first'],
                 'second': x['second'],
@@ -24,10 +33,11 @@ if __name__ == '__main__':
 
         buf.append(mdoc)
 
-        if len(buf) % 10000:
+        if len(buf) % 10000 == 0:
             co_PMI.insert(buf)
             buf = []
+            quick.clear()
 
-    if len(buf) != 0
+    if len(buf) != 0:
         co_PMI.insert(buf)
 
