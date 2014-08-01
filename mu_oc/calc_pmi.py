2fc0cbf6069445abad8ac30c73637af561cfb25c
crazyplum <crazyplum24@gmail.com>
2014-08-01 15:05:15 +0800

    correct pmi formula =__=

--- mu_oc/calc_pmi.py
+++ mu_oc/calc_pmi.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 db = pymongo.Connection('doraemon.iis.sinica.edu.tw')['kimo']
 co_f = db['tfidf.emotermcount']
 co_mc = db['mutual_occurrence']
-co_PMI = db['PMI']
+co_PMI = db['pmi']
 
 def oc(term):
     return sum(map(lambda x: x['count'], co_f.find({'term':term},{'_id':0, 'count':1}).batch_size(40)))
@@ -24,7 +24,7 @@ if __name__ == '__main__':
         quick[x['first']] = oc(x['first']) if quick[x['first']] == 0 else quick[x['first']]
         quick[x['second']] = oc(x['second']) if quick[x['second']] == 0 else quick[x['second']]
 
-        pmi = math.log(x['count'] / quick[x['first']] * quick[x['second']])
+        pmi = math.log(x['count'] / (quick[x['first']] * quick[x['second']]))
         mdoc = {
                 'first': x['first'],
                 'second': x['second'],
